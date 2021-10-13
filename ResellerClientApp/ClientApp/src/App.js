968b423e23d0b066b8da88de3a78b23522f8b4f0
Pawel Chrzaniuk <PCK@kmd.dk>
2021-10-13 22:51:04 +0200

    Code cleanup

--- ResellerClientApp/ClientApp/src/App.js
+++ ResellerClientApp/ClientApp/src/App.js
@@ -2,10 +2,7 @@ import React, { Component } from 'react';
 import { Route } from 'react-router';
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
 import { ProductsFetch } from './components/ProductsFetch';
-
 import './custom.css'
 
 export default class App extends Component {
@@ -15,8 +12,6 @@ export default class App extends Component {
     return (
       <Layout>
         <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data' component={FetchData} />
         <Route path='/product' component={ProductsFetch} />
       </Layout>
     );
